4c7e1a28c946f941d5767cf287e635d88b046111
MrZibbert <mrzibbert@eclipso.eu>
2020-12-25 19:56:34 +0300

    double commit

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,15 +1,15 @@
 version=0.9.3
 apt-get update && apt-get -y upgrade
-apt-get install gcc make git -y
+apt-get update && apt-get install gcc make wget nano tar gzip -y && apt-get install  --reinstall build-essential
 wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
-tar xzf 3proxy-${version}.tar.gz
+tar zxvf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
 cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/mrzibbert/3proxy/raw/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
 wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
